6fef39d9ec226d7c09251a5cf385ca3ac936976d
Anton Maes <antonemaes@gmail.com>
2018-06-09 16:58:35 -0700

    adding js for card

--- public/scripts/payments.js
+++ public/scripts/payments.js
@@ -100,7 +100,7 @@
     currency: 'usd',
     total: {
       label: 'Total',
-      amount: document.getElementById("amount").value;,
+      amount: document.getElementById("amount").value,
     },
     requestShipping: true,
     requestPayerEmail: true,
@@ -113,6 +113,43 @@
       },
     ],
   });
+
+// Code for ccard view
+  // Create an instance of the card Element.
+  var card = elements.create('card', {style: style});
+
+  // Add an instance of the card Element into the `card-element` <div>.
+  card.mount('#card-element');
+
+  // Handle real-time validation errors from the card Element.
+  card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = '';
+    }
+  });
+
+  // Handle form submission.
+  var form = document.getElementById('payment-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+      if (result.error) {
+        // Inform the user if there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to your server.
+        stripeTokenHandler(result.token);
+      }
+    });
+  });
+
+
+
 
   // Callback when a source is created.
   paymentRequest.on('source', async event => {
