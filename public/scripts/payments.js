8508d64ff7c66822e1bc1b89c00a29482e307348
Anton Maes <antonemaes@gmail.com>
2018-06-09 21:23:46 -0700

    removed syle

--- public/scripts/payments.js
+++ public/scripts/payments.js
@@ -365,22 +365,7 @@
 
    // Custom styling can be passed to options when creating an Element.
    // (Note that this demo uses a wider set of styles than the guide below.)
-   var style = {
-     base: {
-       color: '#32325d',
-       lineHeight: '18px',
-       fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-       fontSmoothing: 'antialiased',
-       fontSize: '16px',
-       '::placeholder': {
-         color: '#aab7c4'
-       }
-     },
-     invalid: {
-       color: '#fa755a',
-       iconColor: '#fa755a'
-     }
-   };
+  
 
    // Create an instance of the card Element.
    var card = elements.create('card', {style: style});
