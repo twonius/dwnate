0ea8cacd8b6008eced8bf7c7b916b3ee586e4609
Anton Maes <antonemaes@gmail.com>
2018-06-09 22:21:10 -0700

    comparing to demo

--- public/scripts/payments.js
+++ public/scripts/payments.js
@@ -107,21 +107,22 @@
 
   });
 
-  var style_card = {
+  const style_card = {
     base: {
-      color: '#32325d',
-      lineHeight: '18px',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      iconColor: '#666ee8',
+      color: '#31325f',
+      fontWeight: 400,
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
       fontSmoothing: 'antialiased',
-      fontSize: '16px',
+      fontSize: '15px',
       '::placeholder': {
-        color: '#aab7c4'
-      }
+        color: '#aab7c4',
+      },
+      ':-webkit-autofill': {
+        color: '#666ee8',
+      },
     },
-    invalid: {
-      color: '#fa755a',
-      iconColor: '#fa755a'
-    }
   };
 
 
