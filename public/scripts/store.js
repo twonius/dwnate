8e931c2a981f3b92292df0cf3c39ef91c6cef0f8
Anton Maes <antonemaes@gmail.com>
2018-06-27 21:24:18 -0700

    changed amount

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -79,7 +79,9 @@ class Store {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-          amount: amount
+          amount: 1000
+
+
         }),
       });
       const data = await response.json();
