1b17afd1e834510c2a84eca401109adf918800bd
Anton Maes <antonemaes@gmail.com>
2018-07-20 21:33:07 -0700

    requiring middleware

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var middleware = require("../middleware");
 var { isLoggedIn, checkUsercampaign, checkUserComment, isAdmin, isSafe } = middleware; // destructuring assignment
 
 
