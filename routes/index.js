b78c738694ccdec7bec45b2165679c92665aa19a
Anton Maes <antonemaes@gmail.com>
2018-07-20 21:30:56 -0700

    fixing isloggedin crash

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var { isLoggedIn, checkUsercampaign, checkUserComment, isAdmin, isSafe } = middleware; // destructuring assignment
 
 
 //root route
@@ -74,7 +75,7 @@ router.get("/logout", function(req, res){
 });
 
 // placehodler router
-router.get("/placeholder", isloggedIn,function(req,res){
+router.get("/placeholder",function(req,res){
   res.render("placeholder");
 })
 
